6e15b8e97b9f723ac7ffacc15b832c9931e76c19
Raphael Lopes <ph.campanario07@gmail.com>
2026-03-09 12:00:14 -0300

    add footer structure

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,30 @@
+import Logo from "../assets/logo.svg";
+import Nav from "./Nav";
+
 const Footer = () => (
-  <footer>add footer</footer>
+  <footer>
+    <div>
+      <img src={Logo} alt="Little Lemon" />
+    </div>
+    <div>
+      <div>
+        <h4>Doormat navigation</h4>
+        <Nav />
+      </div>
+      <div>
+        <h4>Contact Info</h4>
+        <p>Some fake address</p>
+        <p>(123) 456-7890</p>
+        <p>someemail@littlelemon.com</p>
+      </div>
+      <div>
+        <h4>Social Media</h4>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>Instagram</p>
+      </div>
+    </div>
+  </footer>
 );
 
 export default Footer;
