936907f98337be089066427688758e696c391157
Raphael Lopes <ph.campanario07@gmail.com>
2026-03-09 16:08:10 -0300

    apply css for hero section

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -1,14 +1,16 @@
+import RestaurantFood from "../assets/restaurant-food.jpg";
+
 const HeroSection = () => (
   <section id="hero">
     <div className="container">
-      <div>
+      <div className="hero-content">
         <h1>Little Lemon</h1>
         <h2>Chicago</h2>
         <p>We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.</p>
         <button>Reserve a Table</button>
       </div>
-      <div>
-        add image
+      <div className="hero-img-wrapper">
+        <img src={RestaurantFood} alt="Chef holding a restaurant food" />
       </div>
     </div>
   </section>
