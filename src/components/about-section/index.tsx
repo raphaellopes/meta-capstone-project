98443e3d69d4296e608d3afbfd9131c06d2c0b74
Raphael Lopes <ph.campanario07@gmail.com>
2026-03-10 17:08:43 -0300

    add about page style

--- src/components/about-section/index.tsx
+++ src/components/about-section/index.tsx
@@ -1,4 +1,6 @@
 import './styles.css';
+import About1Img from '@assets/about-restaurant-a.png';
+import About2Img from '@assets/about-restaurant-b.png';
 
 const AboutSection = () => (
   <section id="about">
@@ -8,10 +10,13 @@ const AboutSection = () => (
         <p className="about-subtitle">Chicago</p>
         <p className="about-description">
           We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
+          We are a family owned Mediterranean restaurant, focused on traditional recipes served with a modern twist.
         </p>
       </div>
       <div className="about-img-wrapper">
-        add image
+        <img className="about-img-top" src={About1Img} alt="Restaurant owner talking to the chef about food" />
+        <img className="about-img-bottom" src={About2Img} alt="Restaurant owner smilling with the chef" />
       </div>
     </div>
   </section>
