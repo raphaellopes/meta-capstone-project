956c676d8a50f15681a7ed40efc5b68e096d46c1
Raphael Lopes <ph.campanario07@gmail.com>
2026-03-10 17:45:28 -0300

    improve header style

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,14 +1,20 @@
 import './styles.css';
 import Logo from "@assets/logo.svg";
+import HamburguerIcon from '@components/icons/hamburguer';
 import Nav from "@components/nav";
 
 const Header = () => (
   <header id="header">
     <div className="container">
-      <div>
-        <img src={Logo} alt="Little Lemon" />
+      <button className="mobile-button-toggle">
+        <HamburguerIcon />
+      </button>
+      <div className="header-content">
+        <div>
+          <img src={Logo} alt="Little Lemon" />
+        </div>
+        <Nav />
       </div>
-      <Nav />
     </div>
   </header>
 );
---
+++ src/components/icons/hamburguer.tsx
@@ -0,0 +1,26 @@
+const HamburguerIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    width="35"
+    height="25"
+    fill="none"
+    viewBox="0 0 35 25"
+    {...props}
+  >
+    <g clipPath="url(#clip0_55_367)">
+      <path
+        fill="#000"
+        fillRule="evenodd"
+        d="M1.346 0C.603 0 0 .622 0 1.389s.603 1.389 1.346 1.389h32.308c.743 0 1.346-.622 1.346-1.39C35 .623 34.397 0 33.654 0zM0 12.5c0-.767.603-1.389 1.346-1.389h32.308c.743 0 1.346.622 1.346 1.389s-.603 1.389-1.346 1.389H1.346C.603 13.889 0 13.267 0 12.5m0 11.111c0-.767.603-1.389 1.346-1.389h32.308c.743 0 1.346.622 1.346 1.39 0 .766-.603 1.388-1.346 1.388H1.346C.603 25 0 24.378 0 23.611"
+        clipRule="evenodd"
+      ></path>
+    </g>
+    <defs>
+      <clipPath id="clip0_55_367">
+        <path fill="#fff" d="M0 0h35v25H0z"></path>
+      </clipPath>
+    </defs>
+  </svg>
+);
+
+export default HamburguerIcon;
