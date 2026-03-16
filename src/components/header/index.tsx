0e5707dc08c6c6fa7bbc5ac18b554deef1e3c6a7
Raphael Lopes <ph.campanario07@gmail.com>
2026-03-16 13:28:52 -0300

    convert header style to modular css and apply mobile design

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,22 +1,43 @@
-import "./styles.css";
+import { useState } from "react";
+
+import styles from "./styles.module.css";
 import Logo from "@assets/logo.svg";
 import HamburguerIcon from "@components/icons/hamburguer";
+import CloseIcon from "@components/icons/close";
 import Nav from "@components/nav";
 
-const Header = () => (
-  <header id="header">
-    <div className="container">
-      <button className="mobile-button-toggle">
-        <HamburguerIcon />
-      </button>
-      <div className="header-content">
-        <a href="/" className="logo-link">
-          <img src={Logo} alt="Little Lemon" />
-        </a>
-        <Nav />
-      </div>
-    </div>
-  </header>
-);
+const Header = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  };
+
+  return (
+    <>
+      <header className={styles.root} id="header">
+        <div className="container">
+          <button
+            className={styles.mobileButtonToggle}
+            onClick={toggleMobileMenu}
+          >
+            {isMobileMenuOpen ? <CloseIcon /> : <HamburguerIcon />}
+          </button>
+          <div className={styles.headerContent}>
+            <a href="/" className="logo-link">
+              <img src={Logo} alt="Little Lemon" />
+            </a>
+            <Nav />
+          </div>
+        </div>
+      </header>
+      {isMobileMenuOpen && (
+        <aside className={styles.mobileNavContainer} id="mobile-menu">
+          <Nav />
+        </aside>
+      )}
+    </>
+  );
+};
 
 export default Header;
