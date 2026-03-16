8ee81609bf4460ede1b0a1295c586a968b214fd6
Raphael Lopes <ph.campanario07@gmail.com>
2026-03-16 13:39:00 -0300

    convert footer css to scoped

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,29 +1,29 @@
-import "./styles.css";
+import styles from "./styles.module.css";
 import Logo from "@assets/logo.svg";
 import Nav from "@components/nav";
 
 const Footer = () => (
-  <footer id="footer">
+  <footer className={styles.root} id="footer">
     <div className="container">
       <a href="/" className="logo-link">
         <img src={Logo} alt="Little Lemon" />
       </a>
-      <div className="footer-content">
-        <div className="footer-content-wrapper">
+      <div className={styles.footerContent}>
+        <div className={styles.footerContentWrapper}>
           <h4>Doormat navigation</h4>
           <Nav />
         </div>
-        <div className="footer-content-wrapper">
+        <div className={styles.footerContentWrapper}>
           <h4>Contact Info</h4>
-          <div className="footer-content-box">
+          <div className={styles.footerContentBox}>
             <p>Some fake address</p>
             <p>(123) 456-7890</p>
             <p>someemail@littlelemon.com</p>
           </div>
         </div>
-        <div className="footer-content-wrapper">
+        <div className={styles.footerContentWrapper}>
           <h4>Social Media</h4>
-          <div className="footer-content-box">
+          <div className={styles.footerContentBox}>
             <p>Facebook</p>
             <p>Twitter</p>
             <p>Instagram</p>
